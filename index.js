393fb1af7733ccc757e09d856ad9aec0f2f499f3
Darthporkius <cholochullo@hotmail.com>
2022-07-27 01:06:14 +0200

    json data is saved on data.txt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const fs = require('fs');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -7,14 +7,17 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.post('/newbooking', function(req, res) {
-    let bookingdata = req.body;
-    console.log(bookingdata);
+    let newBooking = req.body;
+    // Write new booking to data.txt
+    let bookingJSON = fs.readFileSync("data/data.txt", "utf-8");
+    let bookingArray = JSON.parse(bookingJSON);
+    bookingArray.push(newBooking);
+    bookingJSON = JSON.stringify(bookingArray);
+    fs.writeFileSync("data/data.txt", bookingJSON, "utf-8"); 
     res.send("Booking successfull");
 })
 
